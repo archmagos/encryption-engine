c99f5e14ee6e312eadcd33e740b2680bd9e1528f
Fred <fredch87@outlook.com>
2018-09-27 12:47:01 +0100

    Function added to determine whether number is odd or even, for use in mapping string chars in array

--- src/Encrypt.elm
+++ src/Encrypt.elm
@@ -6,3 +6,8 @@ encryptString string =
   string
     |> String.split ""
     |> String.concat
+
+-- For use in mapping the List of string chars, to determine whether char index is odd or even
+
+isOdd number =
+  if modBy 2 number /= 0 then True else False
--- tests/EncryptTest.elm
+++ tests/EncryptTest.elm
@@ -7,11 +7,24 @@ import Test exposing (..)
 
 -- Basic test to check that strings are split and then returned to strings
 
-suite : Test
-suite =
+encrypt : Test
+encrypt =
     describe "encryptString"
       [ test "should return a string" <|
         \_ ->
           encryptString "Apple"
             |> Expect.equal "Apple"
+      ]
+
+odd : Test
+odd =
+    describe "isOdd"
+      [ test "should return true if number is odd" <|
+        \_ ->
+          isOdd 3
+            |> Expect.equal True
+      , test "should return false if number is even" <|
+        \_ ->
+          isOdd 4
+            |> Expect.equal False
       ]
